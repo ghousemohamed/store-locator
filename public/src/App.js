2ede4b16f98f7b9e4e282024004b658af2901706
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2020-01-04 00:49:48 +0300

    Adding MapBox

--- public/src/App.js
+++ public/src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
 
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZ2hvdXNlOTciLCJhIjoiY2s0eW5xNzZkMDJkOTNnc2F1ZDBndzhxZyJ9.ptH7ySBJvuqNYLVu_oKUug"
+});
+const markerUrl ='https://cdn3.vectorstock.com/i/1000x1000/13/62/location-icon-pin-symbol-isolated-on-white-vector-21641362.jpg';
 const App = () => {
   const [store, setStore] = useState([]);
   useEffect(() => {
@@ -10,7 +16,6 @@ const App = () => {
       setStore(temp);
     };
     stores();
-    //es-lint-disable-next-line
   }, []);
   const getStores = async e => {
     e.preventDefault();
@@ -20,6 +25,20 @@ const App = () => {
     <div>
       This is a react application!!!
       <button onClick={e => getStores(e)}>Click me to get results</button>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+      >
+        {/* <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}> */}
+          <Marker coordinates={[-0.2416815, 51.5285582]} anchor="bottom">
+            <img src={markerUrl} style={{width: '50px', height: '50px'}}/>
+          </Marker>
+        {/* </Layer> */}
+      </Map>
+      ;
     </div>
   );
 };
